31f08200a3d0a0b40616c2cd9dae52e697cc5bfa
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-03-27 15:23:10 +0530

    [Loveleen] ADD: Course type added

---
+++ src/com/bridgelabz/universityCourse/CourseType.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.universityCourse;
+
+public abstract class CourseType {
+    private String courseName;
+
+    public CourseType(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ": " + courseName;
+    }
+}
+
+// Specific course types
+class ExamCourse extends CourseType {
+    public ExamCourse(String courseName) {
+        super(courseName);
+    }
+}
+
+class AssignmentCourse extends CourseType {
+    public AssignmentCourse(String courseName) {
+        super(courseName);
+    }
+}
+
+class ResearchCourse extends CourseType {
+    public ResearchCourse(String courseName) {
+        super(courseName);
+    }
+}
+
